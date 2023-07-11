5ae9d7abfb6d9f9402714675436ceadff3b98340
Oleksandr Bondarenko <oleksandr.bondarenko@blueprism.com>
2023-07-11 09:14:18 +0100

    @OA-11: Added serilog with Console configuration to existing services

--- src/api/AuthServer/Gateway/Program.cs
+++ src/api/AuthServer/Gateway/Program.cs
@@ -1,6 +1,7 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using Ocelot.Provider.Consul;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,9 +21,13 @@ var services = builder.Services;
 services.AddOcelot(builder.Configuration)
     .AddConsul();
 
+builder.Host.UseSerilog((context, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration));
+
 var app = builder.Build();
 
 app.UseRouting();
 await app.UseOcelot();
+app.UseSerilogRequestLogging();
 
 app.Run();
