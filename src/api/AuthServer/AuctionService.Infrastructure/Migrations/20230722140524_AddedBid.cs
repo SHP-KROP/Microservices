b5d47cd8c1bd05a6cb63c9e059b914ad06e2c1d5
Oleksandr Bondarenko <oleksandr.bondarenko@blueprism.com>
2023-07-22 15:23:20 +0100

    @OA-24: Added Bid migration

---
+++ src/api/AuthServer/AuctionService.Infrastructure/Migrations/20230722140524_AddedBid.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuctionService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedBid : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bid",
+                schema: "auction",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AuctionItemId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Date = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    ActualPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bid", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Bid_AuctionItem_AuctionItemId",
+                        column: x => x.AuctionItemId,
+                        principalSchema: "auction",
+                        principalTable: "AuctionItem",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bid_AuctionItemId",
+                schema: "auction",
+                table: "Bid",
+                column: "AuctionItemId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bid",
+                schema: "auction");
+        }
+    }
+}
