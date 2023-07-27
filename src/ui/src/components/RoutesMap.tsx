713065ae797fd24bf36f14a53da739ac203097b1
DanilDovhopolyi <ddo2102@gmail.com>
2023-07-27 14:06:27 +0300

    Created interfaces for response auth

--- src/ui/src/components/RoutesMap.tsx
+++ src/ui/src/components/RoutesMap.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import { Routes, Route } from 'react-router-dom';
 import routes from '../routes';
 
 export default function RoutesMap() {
   return (
-    <>
-      <ToastContainer />
-      <Routes>
-        {routes.map(({ key, path, component: Component }) => (
-          <Route key={key} path={path} element={<Component />} />
-        ))}
-      </Routes>
-    </>
+    <Routes>
+      {routes.map(({ key, path, component: Component }) => (
+        <Route key={key} path={path} element={<Component />} />
+      ))}
+    </Routes>
   );
 }
